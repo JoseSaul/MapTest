2c10aa970bde4dc94fe87e6dc56ce3256037f466
JoseSaulRE <josesaulre@gmail.com>
2023-01-19 15:38:44 +0000

    Google Maps implementation

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCf9VTJoxIrkUNcg5w5nS3_ooDp-0taCm8")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
